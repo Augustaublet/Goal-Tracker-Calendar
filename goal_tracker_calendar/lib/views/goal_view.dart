f40179529b0f435a39eac9ca56249e31e71ae7fd
August Aublet <august.aublet@gmail.com>
2023-04-07 11:36:00 +0200

    Fist sketch

---
+++ goal_tracker_calendar/lib/views/goal_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../data/goal.dart';
+import '../data/goal_tracker.dart';
+
+class GoalView extends StatelessWidget {
+  final List<Goal> goals;
+
+  const GoalView({super.key, required this.goals});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          buildGoalTileList(context, goals),
+        ],
+      ),
+    );
+  }
+
+  Widget buildGoalTileList(context, goals) {
+    List<Widget> children = [];
+    for (Goal goal in goals) {
+      children.add(_goalListTile(context, goal));
+    }
+    return Column(
+      children: children,
+    );
+  }
+
+  Widget _goalListTile(context, Goal goal) {
+    return ListTile(
+      title: Text(goal.title),
+      subtitle: Text(goal.howOften),
+      trailing: Text(goal.amount.toString()),
+    );
+  }
+}
